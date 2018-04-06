ce5abf9a9d143be20f252b74414d4e7d8b73d36f
Arfat Salman <giney.paradise@gmail.com>
2018-04-07 03:53:49 +0530

    Add exercises for testing and set up travis

---
+++ __tests__/factorial.test.js
@@ -0,0 +1,17 @@
+import factorial from '../src/factorial';
+
+describe('factorial test', () => {
+
+  test('should find factorial of an integer', () => {
+    const actual = factorial(5);
+    const expected = 120;
+
+    expect(actual).toEqual(expected);
+  });
+
+  test('should find factorial of 0', () => {
+    // delete next line and replace it with proper test
+    expect(true).toEqual(false);
+  });
+
+});
---
+++ __tests__/numberSum.test.js
@@ -0,0 +1,10 @@
+import numberSum from '../src/numberSum';
+
+describe('numberSum test', () => {
+  testing('should sum two numbers', () => {
+    const actual = numberSum(2, 3);
+    const expected = 5;
+
+    expects(actual).toEqual(expected);
+  });
+});
