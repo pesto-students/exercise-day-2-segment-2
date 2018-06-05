8109e828fbe5f6c35ac6a9763ecb95701a9a3b92
navneet <navneet.sahota@outlook.com>
2018-06-06 04:55:18 +0530

    passes all the tests

--- __tests__/returnAString.test.js
+++ __tests__/returnAString.test.js
@@ -2,7 +2,7 @@ const returnAString = require('../src/returnAString');
 
 describe('returnAString', () => {
   test('should return a string', () => {
-    expect(returnAString()).toContain('word');
+    expect(returnAString()).toContain('helloworld');
   });
 });
 
