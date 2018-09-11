5acb64baeb1cb7dbb87527bcb3522a9e36f6b887
Anirudh Nimmagadda <anirudh.nimmagadda@gmail.com>
2018-09-12 03:42:09 +0530

    1. Bugfix: module 'everyNth' was not exporting anything 2. Fixed unit tests

--- __tests__/getName.test.js
+++ __tests__/getName.test.js
@@ -2,13 +2,13 @@ import getName from '../src/getName';
 
 describe('throwError test', () => {
   test('should return string with given name', () => {
-    const actual = getName('Martin Garrix');
+    const actual = getName('Ramu Garrix');
     const expected = /Ramu/;
     expect(actual).toMatch(expected);
   });
 
   test('should throw error if argument is not a string', () => {
-    const expected = 'there was an error';
+    const expected = 'name should be of type string';
     expect(() => getName(123)).toThrow(expected);
   });
 });
--- src/everyNth.js
+++ src/everyNth.js
@@ -1,2 +1,4 @@
 // eslint-disable-next-line
 const everyNth = (string, n) => string.split('').filter((a, i) => i % n === 0).join('');
+
+export default everyNth;
