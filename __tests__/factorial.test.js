0939d8dd6364d5bd0a3dde778748dfc32e0c05f9
vipulrawat <vipulrawat.imi@live.in>
2018-09-12 20:02:15 +0530

    Solutions

--- __tests__/factorial.test.js
+++ __tests__/factorial.test.js
@@ -10,6 +10,6 @@ describe('factorial test', () => {
 
   test('should find factorial of 0', () => {
     // delete next line and replace it with proper test
-    expect(true).toEqual(false);
+    expect(factorial(2)).toEqual(2);
   });
 });
--- __tests__/returnTrue.test.js
+++ __tests__/returnTrue.test.js
@@ -3,7 +3,5 @@ const returnTrue = require('../src/returnTrue');
 describe('returnTrue', () => {
   test('should return true as a boolean', () => {
     expect(returnTrue()).toBe(true);
-    expect(returnTrue()).toBeTruthy();
-    expect(returnTrue()).toBeFalsy();
   });
 });
